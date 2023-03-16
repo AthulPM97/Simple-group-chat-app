9f919e0d8545f8aab23d1ecc642a088d20e93c43
athulpm <97athulpm@gmail.com>
2023-03-16 16:25:56 +0530

    implemented showing messages

--- routes/chat.js
+++ routes/chat.js
@@ -1,14 +1,46 @@
 const express = require("express");
+const fs = require("fs");
 
 const router = express.Router();
 
 router.post("/chat", (req, res, next) => {
-  const script = `<script>localStorage.setItem('username', ${JSON.stringify(
-    req.body.username
-  )});</script>`;
-  res.send(
-    `${script}<form action="/chat" method="POST"><input type="text" name="message"/><button type="submit">send</button></form>`
-  );
+  if (req.body.username) {
+    const script = `<script>localStorage.setItem('username', ${JSON.stringify(
+      req.body.username
+    )});</script>`;
+    fs.writeFile('user.txt', req.body.username, (err) => {
+      console.log('username stored to user.txt');
+    });
+    res.send(
+      `${script}<form action="/chat" method="POST"><input type="text" name="message"/><button type="submit">send</button></form>`
+    );
+  } else {
+    next();
+  }
 });
+
+router.post("/chat", (req, res, next) => {
+  const message = req.body.message;
+  const user = fs.readFileSync('user.txt', 'utf-8', (err,data) => {
+    if(err) console.log('error reading file ' + err);
+    return data;
+  });
+  const newMessage = `${user} : ${message}\n`;
+  fs.appendFileSync('messages.txt', newMessage);
+
+  next();
+});
+
+router.post('/chat', (req, res, next) => {
+  const storedMessages = fs.readFileSync('messages.txt', 'utf-8', (err,data) => {
+    if(err) console.log(err);
+    return data;
+  })
+  console.log(storedMessages);
+  res.send(
+    `${storedMessages}<form action="/chat" method="POST"><input type="text" name="message"/><button type="submit">send</button></form>`
+  );
+})
+
 
 module.exports = router;
